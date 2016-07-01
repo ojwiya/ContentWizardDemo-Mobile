ec4ac5397e114c9db97b3b53c409984f0f462ca2
robertc9 <ojwiya@gmail.com>
2016-07-01 12:13:39 +0000

    lint error fix

--- app/main/services/wizard-serv.js
+++ app/main/services/wizard-serv.js
@@ -4,13 +4,13 @@ angular.module('main')
 
   $log.log('Hello from your Service: Wizard in module main');
 
-var data = [{index:0,name:'card 1',content:'Card 1 content'},
-{index:2,name:'card 2',content:'Card 2 content'}
-{index:3,name:'card 3',content:'Card 3 content'}
-{index:4,name:'card 4',content:'Card 4 content'}
-{index:5,name:'card 5',content:'Card 5 content'}
-{index:6,name:'card 6',content:'Card 6 content'}
-{index:7,name:'card 7',content:'Card 7 content'}
+var data = [{index:0,name:'card 1',content:'Card 1 content'}, 
+{index:2,name:'card 2',content:'Card 2 content'}, 
+{index:3,name:'card 3',content:'Card 3 content'}, 
+{index:4,name:'card 4',content:'Card 4 content'}, 
+{index:5,name:'card 5',content:'Card 5 content'}, 
+{index:6,name:'card 6',content:'Card 6 content'}, 
+{index:7,name:'card 7',content:'Card 7 content'}, 
 {index:8,name:'card 8',content:'Card 18 content'}
 ]
 
