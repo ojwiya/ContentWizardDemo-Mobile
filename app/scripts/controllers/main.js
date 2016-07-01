f5e66c4a5a84885ad3aee48ce13cc387ad3201c9
robertc9 <ojwiya@gmail.com>
2016-07-01 08:28:55 +0000

    First commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name contentwizardwebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the contentwizardwebApp
+ */
+angular.module('contentwizardwebApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
